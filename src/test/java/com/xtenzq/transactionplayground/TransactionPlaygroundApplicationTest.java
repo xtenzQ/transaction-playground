9b8ac38e7748ff6b328d0577337166713400dc8a
Nikita Rusetskii <rusetscky@outlook.com>
2025-07-23 22:20:08 +0200

    remove unused transaction management import and add application context test

---
+++ src/test/java/com/xtenzq/transactionplayground/TransactionPlaygroundApplicationTest.java
@@ -0,0 +1,13 @@
+package com.xtenzq.transactionplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionPlaygroundApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring application context loads successfully
+    }
+}
