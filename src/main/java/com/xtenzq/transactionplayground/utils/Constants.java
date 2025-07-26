1d4e4786c43e5cdcadeb277f1b264f54363c1ce7
Nikita <rusetscky@outlook.com>
2025-07-26 12:26:23 +0200

    Implement `ManualTransactionService` for explicit transaction management (#12)
    * implement ManualAccountService for explicit transaction management with tests
    
    * implement ManualTransactionService for explicit transaction management with manual profile support

--- src/main/java/com/xtenzq/transactionplayground/utils/Constants.java
+++ src/main/java/com/xtenzq/transactionplayground/utils/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String JDK_PROXY_PROFILE = "jdk-proxy";
     public static final String CGLIB_PROFILE = "cglib";
     public static final String ASPECTJ_PROFILE = "aspectj";
+    public static final String MANUAL_PROFILE = "manual";
 }
