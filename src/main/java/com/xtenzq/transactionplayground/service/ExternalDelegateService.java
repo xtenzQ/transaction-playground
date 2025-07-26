42eb3886ebb9db534f7276daedfa1aaa6acff8fd
Nikita <rusetscky@outlook.com>
2025-07-26 14:25:48 +0200

    Implement ExternalDelegateService (#27)
    * implement ExternalDelegateService for delegated transactional operations with tests
    
    * add delegate profile to ExternalDelegateService and its tests

---
+++ src/main/java/com/xtenzq/transactionplayground/service/ExternalDelegateService.java
@@ -0,0 +1,27 @@
+package com.xtenzq.transactionplayground.service;
+
+import static com.xtenzq.transactionplayground.utils.Constants.DELEGATE_PROFILE;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Profile(DELEGATE_PROFILE)
+public class ExternalDelegateService {
+
+    private final AccountService accountService;
+
+    @Transactional
+    public void transfer(Long from, Long to, BigDecimal money) {
+        log.info("Current transaction name is: \"{}\"", TransactionSynchronizationManager.getCurrentTransactionName());
+        // transaction will continue
+        accountService.transfer(from, to, money);
+    }
+}
