aa5b6961506846e129d468f6512551cafe5f2796
Nikita Rusetskii <rusetscky@outlook.com>
2025-07-27 09:01:22 +0200

    refactor ExternalDelegateService to depend on AccountServiceImpl; update ExternalDelegateServiceTest to extend BaseJUnitTest and verify transaction execution via TransactionTracker

--- src/main/java/com/xtenzq/transactionplayground/management/service/ExternalDelegateService.java
+++ src/main/java/com/xtenzq/transactionplayground/management/service/ExternalDelegateService.java
@@ -6,22 +6,17 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import java.math.BigDecimal;
 
 @Slf4j
-@Service
 @RequiredArgsConstructor
 @Profile(DELEGATE_PROFILE)
 public class ExternalDelegateService {
 
-    private final AccountService accountService;
+    private final AccountServiceImpl accountService;
 
     @Transactional
     public void transfer(Long from, Long to, BigDecimal money) {
-        log.info("Current transaction name is: \"{}\"", TransactionSynchronizationManager.getCurrentTransactionName());
-        // transaction will continue
         accountService.transfer(from, to, money);
     }
 }
--- src/test/java/com/xtenzq/transactionplayground/management/service/ExternalDelegateServiceTest.java
+++ src/test/java/com/xtenzq/transactionplayground/management/service/ExternalDelegateServiceTest.java
@@ -2,48 +2,29 @@ package com.xtenzq.transactionplayground.management.service;
 
 import static com.xtenzq.transactionplayground.management.utils.Constants.DELEGATE_PROFILE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.xtenzq.transactionplayground.base.entity.Account;
-import com.xtenzq.transactionplayground.management.service.AccountService;
-import com.xtenzq.transactionplayground.management.service.ExternalDelegateService;
-import com.xtenzq.transactionplayground.base.repository.AccountRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.xtenzq.transactionplayground.BaseJUnitTest;
+import com.xtenzq.transactionplayground.util.service.TransactionTracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import java.math.BigDecimal;
 
 @SpringBootTest
 @ActiveProfiles(DELEGATE_PROFILE)
-public class ExternalDelegateServiceTest {
+public class ExternalDelegateServiceTest extends BaseJUnitTest {
 
     @Autowired
-    private ExternalDelegateService externalDelegateService;
-
-    @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
-    private AccountService accountService;
-
-    private Long fromId;
-    private Long toId;
-
-    @BeforeEach
-    void setUp() {
-        accountRepository.deleteAll();
-        var from = accountRepository.save(new Account("Alice", new BigDecimal("100.0000")));
-        var to = accountRepository.save(new Account("Bob", new BigDecimal("50.0000")));
-        fromId = from.getId();
-        toId = to.getId();
-    }
+    private FirstService firstService;
 
     @Test
-    void testTransactionWorks() {
-        externalDelegateService.transfer(fromId, toId, new BigDecimal("25.00"));
-
-        assertEquals(new BigDecimal("75.0000"), accountService.getBalance(fromId));
-        assertEquals(new BigDecimal("75.0000"), accountService.getBalance(toId));
+    void shouldBeExecuted() throws NoSuchMethodException {
+        firstService.execute();
+        assertEquals(3, TransactionTracker.getHistory().size());
+        assertEquals(FirstService.class.getDeclaredMethod("execute"), TransactionTracker.getHistory().get(0).executingMethod());
+        assertEquals(SecondService.class.getDeclaredMethod("execute"), TransactionTracker.getHistory().get(2).executingMethod());
+        assertTrue(TransactionTracker.getHistory().peek().transactionId().contains("FirstService.execute"));
+        TransactionTracker.getHistory().forEach(i -> assertTrue(i.transactionId().contains("FirstService.execute")));
     }
 }
