ae07412daa06597c6d3c1c5fc6f62d3d2489392c
Nikita Rusetskii <rusetscky@outlook.com>
2025-07-27 09:01:16 +0200

    refactor AccountServiceJdkProxyTest to extend BaseAccountServiceTest; register AccountService bean in JdkDynamicProxyConfig

--- src/main/java/com/xtenzq/transactionplayground/management/config/JdkDynamicProxyConfig.java
+++ src/main/java/com/xtenzq/transactionplayground/management/config/JdkDynamicProxyConfig.java
@@ -2,6 +2,10 @@ package com.xtenzq.transactionplayground.management.config;
 
 import static com.xtenzq.transactionplayground.management.utils.Constants.JDK_PROXY_PROFILE;
 
+import com.xtenzq.transactionplayground.base.repository.AccountRepository;
+import com.xtenzq.transactionplayground.management.service.AccountService;
+import com.xtenzq.transactionplayground.management.service.AccountServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,4 +15,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement // JDK dynamic proxy
 public class JdkDynamicProxyConfig {
 
+    @Bean
+    public AccountService accountService(AccountRepository accountRepository) {
+        return new AccountServiceImpl(accountRepository);
+    }
 }
